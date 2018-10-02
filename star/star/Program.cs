5a010579a041f725985fa9e3d1fb1f0998fbb6eb
Kanan <43238830+KenanBoyukkishiyev1997@users.noreply.github.com>
2018-10-02 17:49:41 +0400

    buldigs rooms and people

---
+++ star/star/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mertebeni elave ele");
+            int floor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("otag elave ele");
+            int room = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("sahibleri elave ele");
+            string[,] people =new string[floor, room] ;
+
+            
+
+            for (int floos = 0; floos < floor; floos++)
+            {
+                for (int i = 0; i < room; i++)
+                {
+                  
+                    
+                        Console.WriteLine($"{floos + 1} -ci mertebenin otag: {i + 1} ");
+                          people[floos, i] = Console.ReadLine();
+                    
+                    
+                }
+            }
+
+            for (int floos = 0; floos < floor; floos++)
+            {
+                for (int i = 0; i < room; i++)
+                {
+
+
+                    Console.WriteLine($"{floos + 1} -ci mertebenin otag: {i + 1} sexsler {people[floos,i]} ");
+                    
+
+
+                }
+            }
+
+
+
+
+
+
+        }
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
